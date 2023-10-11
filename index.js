e478a73508fb7aef7c200e4a4db0d4b9b1d3a904
Lane <ldc0618@gmail.com>
2023-10-11 08:35:26 -0700

    change to async

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const axios = require("axios");
 const { faker } = require("@faker-js/faker");
 
-axios.defaults.baseURL = "http://127.0.0.1:8788/v1";
+const axiosInstance = axios.create({
+  baseURL: "https://api.example.com",
+  proxy: false,
+});
+
+axiosInstance.defaults.baseURL = "http://127.0.0.1:8788/v1";
+// axiosInstance.defaults.baseURL = "https://27a58f99.sonicjs-emq.pages.dev/v1";
+// axiosInstance.defaults.baseURL = "https://demo.sonicjs.com/v1";
+
 customConfig = {
   headers: {
     "Content-Type": "application/json",
@@ -14,39 +22,48 @@ var posts = [];
 var comments = [];
 
 //user - 250k
-//category - 100k
+//category - 250k
 //post - 500k, with 4 categories, 2 comments
 //comments - 1M
 
-const userCount = 0;
-const categoryCount = 1;
+const userCount = 3;
+const categoryCount = 2;
+const postCount = 2;
+const commentCount = 3;
+const categoryToPost = 0;
 
-for (let index = 0; index < userCount; index++) {
-  createUser();
-}
+var userCountCreated = 0;
 
-for (let index = 0; index < categoryCount; index++) {
-  createCategory();
+async function start() {
+  // for (let index = 0; index < userCount; index++) {
+  if (userCountCreated < userCount) {
+    await createUser();
+    userCountCreated++;
+  } else {
+    console.log("done");
+    clearInterval(interval);
+  }
+  // sleep(2000);
+  // }
 }
 
 //user
-
-function createUser() {
+async function createUser() {
   const data = {
     firstName: faker.person.firstName(),
     lastName: faker.person.lastName(),
     email: faker.internet.email(),
     password: faker.internet.password(),
     role: "user",
-    table: "users"
   };
 
-  axios
-    .post(`/content`, { data }, customConfig)
-    .then(function (response) {
+  axiosInstance
+    .post(`/users`, { data }, customConfig)
+    .then(async function (response) {
       users.push(data);
-      console.log(response.status);
-      console.log(response.data);
+      console.log("user", response.status, response.data.id);
+      const userId = response.data.id;
+      await createCategory(userId);
     })
     .catch(function (error) {
       console.log(error);
@@ -54,18 +71,21 @@ function createUser() {
 }
 
 // category
-function createCategory() {
+async function createCategory(userId) {
   const data = {
     title: faker.lorem.words(2),
     body: faker.lorem.words(350),
-    table: "category",
   };
 
-  axios
-    .post(`/content`, { data }, customConfig)
-    .then(function (response) {
+  axiosInstance
+    .post(`/categories`, { data }, customConfig)
+    .then(async function (response) {
       categories.push(data);
-      console.log(response.status);
+      console.log("category", response.status, response.data.id);
+      const categoryId = response.data.id;
+      for (let index = 0; index < postCount; index++) {
+        await createPost(userId, categoryId);
+      }
     })
     .catch(function (error) {
       console.log(error);
@@ -73,19 +93,23 @@ function createCategory() {
 }
 
 // post
-function createPost(userId) {
+async function createPost(userId, categoryId) {
   const data = {
-    title: faker.lorem.lines(),
+    title: faker.lorem.words(5),
     body: faker.lorem.words(1000),
     userId: userId,
-    table: "category",
   };
 
-  axios
-    .post(`/content`, { data }, customConfig)
-    .then(function (response) {
+  axiosInstance
+    .post(`/posts`, { data }, customConfig)
+    .then(async function (response) {
       posts.push(data);
-      console.log(response.status);
+      console.log("post", response.status, response.data.id);
+      const postId = response.data.id;
+      for (let index = 0; index < commentCount; index++) {
+        await createComment(userId, postId);
+      }
+      await createCategoryToPost(postId, categoryId);
     })
     .catch(function (error) {
       console.log(error);
@@ -93,21 +117,49 @@ function createPost(userId) {
 }
 
 // comment
-function createComment(userId, postId) {
+async function createComment(userId, postId) {
+  console.log("createComment", userId, postId);
   const data = {
     body: faker.lorem.words(200),
     userId: userId,
     postId: postId,
-    table: "comments",
   };
 
-  axios
-    .post(`/content`, { data }, customConfig)
+  axiosInstance
+    .post(`/comments`, { data }, customConfig)
     .then(function (response) {
-      comments.push(data);
+      console.log("comment", response.status, response.data.id);
       console.log(response.status);
     })
     .catch(function (error) {
       console.log(error);
     });
 }
+
+// category to post
+async function createCategoryToPost(postId, categoryId) {
+  console.log("createCategoryToPost", postId, categoryId);
+  const data = {
+    categoryId,
+    postId,
+  };
+
+  axiosInstance
+    .post(`/categories-to-posts`, { data }, customConfig)
+    .then(function (response) {
+      console.log("createCategoryToPost", response.status, response.data.id);
+      console.log(response.status);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+}
+
+function sleep(miliseconds) {
+  console.log("sleepting", miliseconds);
+  var currentTime = new Date().getTime();
+
+  while (currentTime + miliseconds >= new Date().getTime()) {}
+}
+
+let interval = setInterval(start, 5000);
